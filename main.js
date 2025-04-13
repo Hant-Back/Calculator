d726d6b78443ce7da27e0fa11eea7444220a5149
Hant-Back <koriakroman@gmail.com>
2025-04-13 10:59:07 +0300

    bug fix

--- main.js
+++ main.js
@@ -11,11 +11,18 @@ function clean() {
 function equal() {
     let exp = display.innerHTML;
     if (exp) {
-        let result = eval(exp);
-        if (result % 1 === 0) { // перевірка, чи є у результаті залишок
-            display.innerHTML = result.toFixed(0) // якщо немає залишку, зводимо до 0 знаків після коми
-        } else {
-            display.innerHTML = result.toFixed(2) // якщо є залишок, зводимо до 2 знаків після коми
+        try {
+            if (exp.includes('/0')) { // перевірка наявності ділення на 0
+                throw new Error("You cannot divide by 0"); // генеруємо помилку
+            }
+            let result = eval(exp);
+            if (result % 1 === 0) {
+                display.innerHTML = result.toFixed(0); // якщо немає залишку
+            } else {
+                display.innerHTML = result.toFixed(2); // якщо є залишок
+            }
+        } catch (error) {
+            display.innerHTML = error.message; // виведення повідомлення про помилку
         }
     }
 }

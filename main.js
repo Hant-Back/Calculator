7b775e277498f79db014761906d2ce5f88627448
Hant-Back <160278129+Hant-Back@users.noreply.github.com>
2025-04-14 10:38:50 +0300

    Update main.js

--- main.js
+++ main.js
@@ -13,21 +13,16 @@ function equal() {
     if (exp) {
         try {
             if (exp.includes('/0')) { // перевірка наявності ділення на 0
-                throw new Error("You cannot divide by 0"); // генеруємо помилку
+                throw new Error("На 0 ділити не можна"); // генеруємо помилку
+            }
+            if (/[\+\-\*\/]{2,}/.test(exp)) { // перевірка на недопустимі комбінації операторів
+                throw new Error("Недопустима комбінація операторів"); // генеруємо помилку
             }
             let result = eval(exp);
-            let hint1 = "卐"
-            let hint2 = "ꑭ Азов"
             if (result % 1 === 0) {
                 display.innerHTML = result.toFixed(0); // якщо немає залишку
             } else {
                 display.innerHTML = result.toFixed(2); // якщо є залишок
-            }
-            if (result === 1488) {
-                display.innerHTML = hint1;
-            }
-            if (result === 4308) {
-                display.innerHTML = hint2;
             }
         } catch (error) {
             display.innerHTML = error.message; // виведення повідомлення про помилку
